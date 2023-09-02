97cb075a78ff868c1e2a5804e814152919792482
shivamkumarjha03 <110899491+shivamkumarjha03@users.noreply.github.com>
2023-09-02 11:49:52 +0530

    adding two number
    addition

---
+++ src/Addnumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+        public class Addnumbers
+        {
+            public static void main(String args[])
+            {
+                int a,b,c;
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter first number:");
+                a=sc.nextInt();
+                System.out.println("Enter second number:");
+                b=sc.nextInt();
+                c=a+b;
+                System.out.println("Addition="+c);
+            }
+        }
